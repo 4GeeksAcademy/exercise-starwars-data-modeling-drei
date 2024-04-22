781087e6be21e562dd5937ab89c6e451022e931d
DreiSup <153184780+DreiSup@users.noreply.github.com>
2024-04-22 20:30:40 +0000

    starwars modelo de datos by drei

--- src/models.py
+++ src/models.py
@@ -7,23 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(25), nullable=False) 
+    subscription = Column(String(250), nullable=False) 
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Planets = Column(String)
+    Characters = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    diameter = Column(String(250))
+    films = Column(String)
+    gravity = Column(String(250))
+    population = Column(String(250))
+    rotation_period = Column(String(250))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    birth_year = Column(Integer)
+    eye_color = Column(String)
+    gender = Column(String)
+    height = Column(Integer)
+    mass = Column(Integer)
+
+
 
     def to_dict(self):
         return {}
